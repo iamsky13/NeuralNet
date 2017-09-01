377db4d2af095d0680e62d87a1f34f239b8ac074
Aakash <uniqueaakash13@hotmail.com>
2017-09-01 22:12:20 +0545

    input output seed

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,1 +1,13 @@
-import numpy as numpy
+import numpy as np
+
+X = np.array([[0,0,1],  
+            [0,1,1],
+            [1,0,1],
+            [1,1,1]]) 
+
+Y = np.array([[0],
+              [1],
+              [1],
+              [0]])
+
+np.random.seed(1)
